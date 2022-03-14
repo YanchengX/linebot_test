0357d5d57c978d0699e6419f4c39a387cb0618e9
Yancheng-chen <51571103+YanchengX@users.noreply.github.com>
2022-03-14 20:05:49 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 # 載入LineBot所需要的模組
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 import os
 from flask import Flask, request, abort
 
@@ -9,19 +12,11 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-
 app = Flask(__name__)
 
-# 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi(
     'Un/J4DSchKvjhWr8uvEmXUKOowijPJCpiWU77iZxRxa8PgTRiBtrk4CHhlog7DGtHBh3t7Ato6+Bh+j43UPaTfSs6SireSV0dhnAFM+xqcR4neWzcZtzcksdub+lWtsugybkeCproHycuDT9vRUGggdB04t89/1O/w1cDnyilFU=')
-
-# 必須放上自己的Channel Secret
 handler = WebhookHandler('7cd651ab928ae756ce8225a6696a4a50')
-
-line_bot_api.push_message('你自己的ID', TextSendMessage(text='你可以開始了'))
-
-# 監聽所有來自 /callback 的 Post Request
 
 
 @app.route("/callback", methods=['POST'])
@@ -37,20 +32,18 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
 
 
-# 訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 
-# 主程式
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
