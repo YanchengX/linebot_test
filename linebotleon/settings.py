fdc8f3a26dc5218871c752697089aac948cf9d5c
Yancheng-chen <jaeez89628@gmail.com>
2022-04-02 20:11:50 +0800

    sen

--- linebotleon/settings.py
+++ linebotleon/settings.py
@@ -126,9 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICILES_DIRS = [
-    os.path.join(BASE_DIR,'static')#加入static路徑
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
